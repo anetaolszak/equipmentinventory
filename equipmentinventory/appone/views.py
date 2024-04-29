c2f4011f7f4b98ffc4994c3c6020dfb31e1492af
anetaolszak <w1831445@my.westminster.ac.uk>
2024-04-29 12:26:33 +0100

    I created functions to generate various types of reports, such as current inventory status, equipment usage history, warranty, overdue equipment etc.

--- equipmentinventory/appone/views.py
+++ equipmentinventory/appone/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+
 from .models import Equipment, DeviceWarranty, UsageHistory
 from .forms import CreateItemForm, ItemForm
 from django.utils import timezone
